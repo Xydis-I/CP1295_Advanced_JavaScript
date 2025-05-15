46e73cdb10ab75bb38008b529d9c5e2fafa58e1e
Xydis <ccb9952@hotmail.com>
2025-05-15 11:50:50 -0230

    Cafe question done.

--- 2025-05-14/Project 6-4/cafe.js
+++ 2025-05-14/Project 6-4/cafe.js
@@ -1,15 +1,53 @@
 "use strict";
-const getElement = selector => document.querySelector(selector);
+const $ = selector => document.querySelector(selector);
 
 const getSelectedProduct = src => {
     
 };
 
 document.addEventListener("DOMContentLoaded", () => {
-   const images = document.getElementById("menu-list");
+   const images = $("#menu-list").children;
+   let total = 0;
 
-   for (let image of images) {
-    
+   const prices = {
+      "espresso":1.95,
+      "latte":3.15,
+      "cappuccino":3.45,
+      "coffee":2.15,
+      "biscotti":2.90,
+      "scone":2.95
    }
 
+   for (let image of images) {
+      image = image.getElementsByTagName("img")[0];
+
+      let info = image.id;
+      let original = image.src;
+
+      image.addEventListener("mouseover", () =>{
+         image.src = info;
+      });
+
+      image.addEventListener("mouseout", () =>{
+         image.src = original;
+      });
+
+      image.addEventListener("click", () => {
+         let option = document.createElement("option");
+         option.innerHTML = `$${prices[`${image.alt}`].toFixed(2)} - ${image.alt.charAt(0).toUpperCase() + image.alt.slice(1)}`;
+         document.getElementById("order").appendChild(option);
+         total += prices[`${image.alt}`];
+         document.getElementById("total").innerHTML = `Total: $${total.toFixed(2)}`;
+      });
+   }
+
+   $("#place_order").addEventListener("click", () => {
+      window.location.href = "checkout.html";
+   });
+
+   $("#clear_order").addEventListener("click", () => {
+      $("#order").innerHTML = "";
+      total = 0;
+      document.getElementById("total").innerHTML = "";
+   });
 }); 
