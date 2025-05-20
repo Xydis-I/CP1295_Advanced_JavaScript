a9a2dd5ece8277e4654627c881be2b1cce665bbe
C. C. Barrett <ccb9952@hotmail.com>
2025-05-20 03:12:17 -0230

    countdown finished

--- 2025-05-14/Project 8-1/countdown.js
+++ 2025-05-14/Project 8-1/countdown.js
@@ -4,48 +4,76 @@ const getElement = selector => document.querySelector(selector);
 
 document.addEventListener("DOMContentLoaded", () => {
 
+    let timer;
+
     getElement("#countdown").addEventListener("click", () => {
-        const eventName = getElement("#event").value;
-        const eventDateString = getElement("#date").value;  
-        const messageLbl = getElement("#message");  
+        clearInterval(timer);
 
-        // make sure user entered event and date 
-        if (eventName == "" || eventDateString == "") {
-            messageLbl.textContent = "Please enter both a name and a date.";
-            return;
-        }
+        updateClock();
 
-        // convert event date string to Date object and check for validity
-        const eventDate = new Date(eventDateString);
-        if (eventDate.toString() == "Invalid Date") {
-            messageLbl.textContent = "Please enter a valid date.";
-            return;
-        }
+        timer = setInterval(() => {
+            updateClock();
+        }, 1000);
+    });
 
-        // calculate days
-        const today = new Date();
-        const msFromToday = eventDate.getTime() - today.getTime();
-        const msForOneDay = 24 * 60 * 60 * 1000; // hrs * mins * secs * milliseconds  
-        const daysToDate = Math.ceil( msFromToday / msForOneDay ); 
+    getElement("#event").addEventListener("click", () => {
+        clearInterval(timer);
+    });
 
-        //calculate hours
-        let hoursToDate = Math.ceil( (msFromToday - daysToDate * msForOneDay) / 60 / 3600 )
-        console.log(hoursToDate);
-
-        // create and display message 
-        const displayDate = eventDate.toDateString();
-        let msg = "";
-        if (daysToDate == 0) {
-            msg = `Hooray! Today is ${eventName}! (${displayDate})`;
-        } else if (daysToDate > 0) {
-            msg = `${daysToDate} day(s) until ${eventName}! (${displayDate})`;
-        } else if (daysToDate < 0) {
-            msg = `${eventName} happened ${Math.abs(daysToDate)} 
-                   day(s) ago. (${displayDate})`;
-        }
-        messageLbl.textContent = msg;
+    getElement("#date").addEventListener("click", () => {
+        clearInterval(timer);
     });
 
     // set focus on first text box
     getElement("#event").focus();
 });
+
+function updateClock() {
+    const eventName = getElement("#event").value;
+    const eventDateString = getElement("#date").value;  
+    const messageLbl = getElement("#message");  
+
+    // make sure user entered event and date 
+    if (eventName == "" || eventDateString == "") {
+        messageLbl.textContent = "Please enter both a name and a date.";
+        return;
+    }
+
+    // convert event date string to Date object and check for validity
+    const eventDate = new Date(eventDateString);
+    if (eventDate.toString() == "Invalid Date") {
+        messageLbl.textContent = "Please enter a valid date.";
+        return;
+    }
+
+    // calculate days
+    const today = new Date();
+    let msFromToday = eventDate.getTime() - today.getTime();
+    const msForOneDay = 24 * 60 * 60 * 1000; // hrs * mins * secs * milliseconds  
+    const daysToDate = Math.floor( msFromToday / msForOneDay ); 
+
+    //calculate hours
+    msFromToday -= (daysToDate * 24 * 60 * 60 * 1000);
+    let hoursToDate = Math.floor( (msFromToday) / 1000 / 3600 );
+
+    //calculate minutes
+    msFromToday -= (hoursToDate * 60 * 60 * 1000);
+    let minutesToDate = Math.floor( (msFromToday) / 1000 / 60 );
+
+    //calculate seconds
+    msFromToday -= (minutesToDate * 60 * 1000);
+    let secondsToDate = Math.floor( (msFromToday) / 1000 );
+
+    // create and display message 
+    const displayDate = eventDate.toDateString();
+    let msg = "";
+    if (daysToDate == 0) {
+        msg = `Hooray! Today is ${eventName}! (${displayDate})`;
+    } else if (daysToDate > 0) {
+        msg = `${daysToDate} day(s), ${hoursToDate} hours, ${minutesToDate} minutes, ${secondsToDate} seconds until ${eventName} (${displayDate})`;
+    } else if (daysToDate < 0) {
+        msg = `${eventName} happened ${Math.abs(daysToDate)} 
+                day(s) ago. (${displayDate})`;
+    }
+    messageLbl.textContent = msg;
+}
