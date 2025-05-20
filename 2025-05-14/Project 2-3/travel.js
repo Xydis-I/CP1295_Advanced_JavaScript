0e8ae11ec5684f74e020cf00cb650dc650a0c576
C. C. Barrett <ccb9952@hotmail.com>
2025-05-20 03:30:56 -0230

    Review Assignment Complete.

--- 2025-05-14/Project 2-3/travel.js
+++ 2025-05-14/Project 2-3/travel.js
@@ -1,11 +1,11 @@
 "use strict";
 
-let distance = parseInt(prompt("Distance: "));
-let speed = parseInt(prompt("Speed: "));
+let distance = parseInt(prompt("Distance (miles): "));
+let speed = parseInt(prompt("Speed (MPH): "));
 
 let hours = Math.trunc((distance / speed))
 let minutes = ((distance / speed) - hours) * 60;
 
 alert(`Distance: ${distance} miles
-    \nSpeed: ${speed} MPH}
+    \nSpeed: ${speed} MPH
     \nTravel time: ${hours} hours, ${Math.round(minutes)} minutes`);
