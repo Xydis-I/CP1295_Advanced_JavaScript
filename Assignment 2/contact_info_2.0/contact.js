08a77c678e36eaea8450bcfd01613d252cc26765
C. C. Barrett <ccb9952@hotmail.com>
2025-05-26 01:01:48 -0230

    Project questions complete, 2/4.

--- Assignment 2/contact_info_2.0/contact.js
+++ Assignment 2/contact_info_2.0/contact.js
@@ -1,3 +1,68 @@
 "use strict";
-const getElement = selector => document.querySelector(selector);
+const $ = selector => document.querySelector(selector);
 
+document.addEventListener("DOMContentLoaded", () => {
+    $("#name").addEventListener("invalid", () => {
+        $("#name").nextElementSibling.textContent = "Please enter your name.";
+        $("#name").focus();
+    });
+
+    $("#email").addEventListener("invalid", () => {
+        $("#email").nextElementSibling.textContent = "Please enter an email or phone.";
+        $("#email").focus();
+    });
+
+    $("#phone").addEventListener("invalid", () => {
+        $("#email").nextElementSibling.textContent = "Please enter an email or phone.";
+        $("#email").focus();
+    });
+
+    $("#zip").addEventListener("invalid", () => {
+        $("#zip").nextElementSibling.textContent = "Please enter a 5 digit ZIP.";
+        $("#zip").focus();
+    });
+
+    $("#dob").addEventListener("invalid", () => {
+        $("#dob").nextElementSibling.textContent = "Please enter a date of birth.";
+        $("#dob").focus();
+    });
+
+    $("form").addEventListener("submit", evt => {
+
+        const emailPattern = /\S+@\S+\.\S+/;
+        const phonePattern = /\d{3}[\-]\d{3}[\-]\d{4}/;
+        
+        const email = $("#email");
+        const phone = $("#phone");
+
+        let dob = new Date($("#dob").value + "T00:00:00");
+
+        let today = new Date();
+        today.setHours(0,0,0,0);
+
+        console.log(dob >= today);
+
+        if (!$("form").checkValidity() || (!emailPattern.test(email.value) && !phonePattern.test(phone.value)) || dob >= today) {
+            if (!emailPattern.test(email.value) && !phonePattern.test(phone.value)) {
+                email.nextElementSibling.textContent = "Please enter an email or phone.";
+            } else {
+                email.nextElementSibling.textContent = "";
+            }
+
+            if (dob >= today) {
+                $("#dob").nextElementSibling.textContent = "DOB must be in the past.";
+            } else if ($("#dob").checkValidity()) {
+                $("#dob").nextElementSibling.textContent = "";
+            }
+
+            evt.preventDefault();
+        }
+    });
+
+    $("form").addEventListener("reset", () => {
+        let elements = $("form").elements;
+        for (let index = 0; index < 5; index++) {
+            elements[index].nextElementSibling.textContent = "";
+        }
+    });
+});
