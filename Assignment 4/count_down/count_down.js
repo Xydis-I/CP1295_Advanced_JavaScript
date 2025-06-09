ef72f076dde708e68fee38b8200680f6b9ee6836
C. C. Barrett <ccb9952@hotmail.com>
2025-06-09 19:35:40 -0230

    Assignment 4 Book Question 1 complete.

--- Assignment 4/count_down/count_down.js
+++ Assignment 4/count_down/count_down.js
@@ -4,41 +4,23 @@ const getElement = selector => document.querySelector(selector);
 
 document.addEventListener("DOMContentLoaded", () => {
     getElement("#countdown").addEventListener("click", () => {
-        const eventName = getElement("#event").value;
-        const eventDateString = getElement("#date").value;  
+        let event = new MyEvent(getElement("#event").value, getElement("#date").value)
+
         const messageLbl = getElement("#message");  
 
         // make sure user entered event name and date 
-        if (eventName.length == 0 || eventDateString.length == 0) {
+        if (!event.hasName() || !event.hasDate()) {
             messageLbl.textContent = "Please enter both a name and a date.";
             return;
         }
 
         // make sure date is valid
-        const eventDate = new Date(eventDateString);
-        if (eventDate == "Invalid Date") {
+        if (!event.isValidDate()) {
             messageLbl.textContent = "Please enter a valid date.";
             return;
         }
-
-        // calculate days
-        const today = new Date();
-        const msFromToday = eventDate.getTime() - today.getTime();
-        const msForOneDay = 24 * 60 * 60 * 1000; // hrs * mins * secs * milliseconds  
-        const daysToDate = Math.ceil( msFromToday / msForOneDay ); 
-
-        // create and display message 
-        let msg = "";
-        const date = eventDate.toDateString();
-        if (daysToDate == 0) {
-            msg = `Hooray! Today is ${eventName}! (${date})`;
-        } else if (daysToDate > 0) {
-            msg = `${daysToDate} day(s) until ${eventName}! (${date})`;
-        } else if (daysToDate < 0) {
-            msg = `${eventName} happened ${Math.abs(daysToDate)} 
-                   day(s) ago. (${date})`;
-        }
-        messageLbl.textContent = msg;
+        
+        messageLbl.textContent = event.message;
     });
 
     // set focus on first text box
---
+++ Assignment 4/count_down/lib_event.js
@@ -0,0 +1,46 @@
+class MyEvent {
+    name;
+    date;
+    #datestring;
+
+    constructor(name, datestring) {
+        this.name = name;
+        this.#datestring = datestring;
+        this.date = new Date(datestring);
+    }
+
+    hasName() {
+        return this.name.length > 0;
+    }
+
+    hasDate() {
+        return this.#datestring.length > 0;
+    }
+
+    isValidDate() {
+        return this.date.toString() != "Invalid Date"
+    }
+
+    get days() {
+        // calculate days
+        const today = new Date();
+        const msFromToday = this.date.getTime() - today.getTime();
+        const msForOneDay = 24 * 60 * 60 * 1000; // hrs * mins * secs * milliseconds  
+        return Math.ceil( msFromToday / msForOneDay ); 
+    }
+
+    get message() {
+        // create and display message 
+        let msg = "";
+        const date = this.date.toDateString();
+        if (this.days == 0) {
+            msg = `Hooray! Today is ${this.name}! (${date})`;
+        } else if (this.days > 0) {
+            msg = `${this.days} day(s) until ${this.name}! (${date})`;
+        } else if (this.days < 0) {
+            msg = `${this.name} happened ${Math.abs(this.days)} 
+                   day(s) ago. (${date})`;
+        }
+        return msg;
+    }
+}
